18094797a198a85ad0cb43175bda882937b6e809
Yueying <wangyy395@gmail.com>
2020-06-13 17:00:46 -0400

    style homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,15 @@ Area.destroy_all
 wyy = User.create(name: 'wyy', password: '123', zipcode: '07093')
 leo = User.create(name: 'leo', password: '123', zipcode: '07093')
 lucky = User.create(name: 'lucky', password: '123', zipcode: '07093')
-haruki = User.create(name: 'haruki', password: '123', zipcode: '07093')
-spike = User.create(name: 'spike', password: '123', zipcode: '10010')
+haruki = User.create(name: 'haruki', password: '123', zipcode: '07093') #wny
+spike = User.create(name: 'spike', password: '123', zipcode: '10010') #Stuyvesant
+user1 = User.create(name: 'amy', password: '123', zipcode: '07087') #union city
+user3 = User.create(name: 'chen', password: '123', zipcode: '07310') #jersey city
+user4 = User.create(name: 'greg', password: '123', zipcode: '07030') #hoboken
+user5 = User.create(name: 'godiva', password: '123', zipcode: '07086') #weehawken
+user6 = User.create(name: 'sophia', password: '123', zipcode: '10001') #manhattan
+user6 = User.create(name: 'joey', password: '123', zipcode: '11101') #queens
+
 
 pet = Category.create(name: 'pet adoption')
 housing = Category.create(name: 'housing')
@@ -26,14 +33,19 @@ education = Category.create(name: 'education')
 event = Category.create(name: 'event')
 
 wny = Area.create(zipcode: '07093', name: 'West New York')
-lic = Area.create(zipcode: '10010', name: 'Stuyvesant Park')
+stuyvesant = Area.create(zipcode: '10010', name: 'Stuyvesant Park')
 union_city = Area.create(zipcode: '07087', name: 'Union City')
+jersey_city = Area.create(zipcode: '07310', name: 'Jersey City')
+hoboken = Area.create(zipcode: '07030', name: 'Hoboken')
+weehawken = Area.create(zipcode: '07086', name: 'Weehawken')
+manhattan = Area.create(zipcode: '10001', name: 'Manhattan')
+queens = Area.create(zipcode: '11101', name: 'Queens')
 
 post1 = Post.create(title: 'sell switch', description: 'i want to sell my switch!', user: wyy, area: wny, category: marketplace)
 post2 = Post.create(title: 'teach python', description: 'i am professor python', user: leo, area: wny, category: education)
 post3 = Post.create(title: 'baby dog adoption', description: 'cute baby dogs for adoption!', user: lucky, area: wny, category: pet)
 post4 = Post.create(title: 'dog show', description: 'welcome all dogs to central park', user: haruki, area: wny, category: event)
-post5 = Post.create(title: 'housing', description: 'buy me a new dog home, i tear mine', user: spike, area: lic, category: housing)
+post5 = Post.create(title: 'housing', description: 'buy me a new dog home, i tear mine', user: spike, area: stuyvesant, category: housing)
 
 comment1 = Comment.create(content: 'how much?', user: lucky, post: post1)
 comment2 = Comment.create(content: 'avaliable now', user: wyy, post: post2)
